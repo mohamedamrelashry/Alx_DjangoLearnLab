b2754b82b9ec727bc6d6d085094a2224bfa52333
mohamedamrelashry <amrelashry8000@gmail.com>
2025-08-30 00:01:08 +0300

    bbb

---
+++ django-models/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # الأعمدة اللي تظهر في صفحة القائمة
+    list_display = ('title', 'author', 'publication_year')
+
+    # إضافة فلتر على جانب الصفحة
+    list_filter = ('author', 'publication_year')
+
+    # تمكين البحث
+    search_fields = ('title', 'author')
+from django.contrib import admin
+
+# Register your models here.
