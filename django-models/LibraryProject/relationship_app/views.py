712fa2aaee0a5e510041a93d3460c2b50a2c9ba1
mohamedamrelashry <156803087+mohamedamrelashry@users.noreply.github.com>
2025-08-30 01:07:48 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,5 +8,5 @@ def list_books(request):
     return render(request, 'relationship_app/list_books.html', {'books': books})
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = '"relationship_app/library_detail.html'
     context_object_name = 'library'
